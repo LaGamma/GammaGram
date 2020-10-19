26881da04a6a1690d28b89f8751ca5f590368ef8
LaGamma <nplagamma@email.wm.edu>
2020-10-19 04:03:50 -0400

    allowed registration of new accounts

--- app/src/main/java/com/nicolaslagamma/gammagram/ui/login/LoginViewModel.java
+++ app/src/main/java/com/nicolaslagamma/gammagram/ui/login/LoginViewModel.java
@@ -15,6 +15,7 @@ import com.nicolaslagamma.gammagram.R;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import java.util.regex.Pattern;
 
@@ -52,15 +53,38 @@ public class LoginViewModel extends ViewModel {
                         LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
                         loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
                     } else {
-                        // User did not confirm the e-mail!!
+                        // User did not confirm the e-mail!! (Let them log in anyway)
                         Result<LoggedInUser> result = loginRepository.login(username, password);
                         LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
                         loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
                     }
                 } else {
-                    // TODO: Better error handling
+                    createNewUser(username, password);
+                }
+            }
+        });
+    }
+
+    public void createNewUser(final String username, final String password) {
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        //user.setEmail(userName);
+
+        // Other fields can be set just like any other ParseObject,
+        // using the "put" method, like this: user.put("attribute", "its value");
+        // If this field does not exists, it will be automatically created
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    Log.i("LoginViewModel", "new user " + username + " created!");
+                    Result<LoggedInUser> result = loginRepository.login(username, password);
+                    LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
+                    loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
+                } else {
                     // Signup failed. Look at the ParseException to see what happened.
-                    Log.e("LoginDataSource", e.toString());
+                    Log.e("LoginViewModel", e.toString());
                     loginResult.setValue(new LoginResult(R.string.login_failed));
                     return;
                 }
