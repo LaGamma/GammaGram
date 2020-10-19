9b01daa5ada5bd931b90bdc5976dfe6319cd0197
LaGamma <nplagamma@email.wm.edu>
2020-10-18 22:35:58 -0400

    login authentication setup

--- app/src/main/java/com/nicolaslagamma/gammagram/ui/login/LoginActivity.java
+++ app/src/main/java/com/nicolaslagamma/gammagram/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -22,6 +23,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.nicolaslagamma.gammagram.MainActivity;
 import com.nicolaslagamma.gammagram.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -120,6 +122,8 @@ public class LoginActivity extends AppCompatActivity {
     private void updateUiWithUser(LoggedInUserView model) {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
+        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(i);
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
     }
 
--- app/src/main/java/com/nicolaslagamma/gammagram/ui/login/LoginViewModel.java
+++ app/src/main/java/com/nicolaslagamma/gammagram/ui/login/LoginViewModel.java
@@ -4,12 +4,16 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import android.util.Log;
 import android.util.Patterns;
 
 import com.nicolaslagamma.gammagram.data.LoginRepository;
 import com.nicolaslagamma.gammagram.data.Result;
 import com.nicolaslagamma.gammagram.data.model.LoggedInUser;
 import com.nicolaslagamma.gammagram.R;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginViewModel extends ViewModel {
 
@@ -29,16 +33,21 @@ public class LoginViewModel extends ViewModel {
         return loginResult;
     }
 
-    public void login(String username, String password) {
-        // can be launched in a separate asynchronous job
-        Result<LoggedInUser> result = loginRepository.login(username, password);
-
-        if (result instanceof Result.Success) {
-            LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
-            loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
-        } else {
-            loginResult.setValue(new LoginResult(R.string.login_failed));
-        }
+    public void login(final String username, final String password) {
+        // launch in a separate asynchronous job
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e("LoginDataSource", "Issue with login", e);
+                    loginResult.setValue(new LoginResult(R.string.login_failed));
+                    return;
+                }
+                Result<LoggedInUser> result = loginRepository.login(username, password);
+                LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
+                loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
+            }
+        });
     }
 
     public void loginDataChanged(String username, String password) {
